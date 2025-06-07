00fc9426d459081874308f0e2d21cf927d0665a6
Elliot Wargo <49532242+Elliot2718@users.noreply.github.com>
2025-06-07 06:33:39 -0700

    don't have python run in background

--- src/broker/start_mqtt_services.sh
+++ src/broker/start_mqtt_services.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# Wait for Mosquitto to initialize
 sleep 2
-
-# Change to your project directory
 cd /home/elliot.wargo/drippy/src/broker || exit
-
-# Activate virtual environment and run script in the same shell
 source env/bin/activate
-python3 mqtt_to_sqlite.py &
+
+# Run Python in foreground so systemd knows the service is running
+python3 mqtt_to_sqlite.py >> /home/elliot.wargo/drippy/mqtt.log 2>&1
